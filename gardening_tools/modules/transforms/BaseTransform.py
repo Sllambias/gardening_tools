06230a81f8c1db7ab3db04cf941b1b95e5b49452
Sllambias <llambias@live.com>
2025-12-10 15:56:00 +0100

    add dict to basexform

--- gardening_tools/modules/transforms/BaseTransform.py
+++ gardening_tools/modules/transforms/BaseTransform.py
@@ -9,9 +9,8 @@ class BaseTransform(object):
         """This will be called during __call__ and can be used to retrieve arbitrary parameters for the transform applied during __call__."""
 
     @abc.abstractmethod
-    def __call__(self):
+    def __call__(self, dict):
         """
         This will be of the form __call__(self, dict):
-        which allows calling it as either transform(data_dict) or transform(**data_dict),
-        supporting both Torch pipelines and batchgenerators.
+        which allows calling it as either transform(dict)
         """
